2d45ec4e53e61f127f48b737052324d7993c21f9
Transluciddata <mcifuent@gmail.com>
2018-10-29 16:05:41 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let lArrow = document.querySelector('.lArrow');
+let rArrow = document.querySelector('.rArrow');
+let images = document.querySelectorAll('img');
+let count = 0;
+
+
+rArrow.addEventListener('click', () => {
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.transform=`translateX(-100%)`;
+    }                
+})
+
+lArrow.addEventListener('click', () => {
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.transform=`translateX(-100%)`;
+    }                
+})
